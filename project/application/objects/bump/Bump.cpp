ce163cb4695ac323428d20e35c4e0d7776b23c79
KazukiIhara <iharakazuki0829@gmail.com>
2025-01-13 21:04:25 +0900

    空のたんこぶクラス実装

---
+++ project/application/objects/bump/Bump.cpp
@@ -0,0 +1,7 @@
+#include "Bump.h"
+
+void Bump::Initialize(const std::string& name) {}
+
+void Bump::Update() {}
+
+void Bump::OnCollision(Collider* other) {}
--- project/application/objects/bump/Bump.h
+++ project/application/objects/bump/Bump.h
@@ -3,10 +3,18 @@
 #include "3d/entityController/EntityController.h"
 
 // たんこぶクラス
-class Bump {
+class Bump:public EntityController {
 public:
 	Bump() = default;
 	~Bump() = default;
+
+	void Initialize(const std::string& name)override;
+
+	// 更新
+	void Update()override;
+
+	// 衝突コールバック関数
+	void OnCollision([[maybe_unused]] Collider* other)override;
 
 private:
 	// 大きさ
