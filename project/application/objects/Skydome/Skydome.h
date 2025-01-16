243990f41d8ff189406ebda0fd6cd3341854dd4f
KazukiIhara <k023g0076@g.neec.ac.jp>
2025-01-16 14:51:21 +0900

    天球クラスを作成

---
+++ project/application/objects/Skydome/Skydome.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "3d/entityController/EntityController.h"
+
+class Skydome : public EntityController {
+public:
+	Skydome() = default;
+	~Skydome() = default;
+
+	void Initialize(const std::string& name)override;
+
+	// 更新
+	void Update()override;
+
+	// 衝突コールバック関数
+	void OnCollision([[maybe_unused]] Collider* other)override;
+
+private:
+
+	float speed_ = 0.0001f;
+
+};
