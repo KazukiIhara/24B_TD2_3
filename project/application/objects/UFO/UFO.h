715bf0c26e656c2c201bb1d8faeb390e07a3cb9c
KazukiIhara <iharakazuki0829@gmail.com>
2025-02-02 17:13:57 +0900

    UFOクラス途中まで

--- project/application/objects/UFO/UFO.h
+++ project/application/objects/UFO/UFO.h
@@ -5,10 +5,40 @@
 #include <optional>
 
 class UFO: public EntityController {
+	enum class Behavior {
+		kRoot,
+	};
 public:
 	UFO();
 	~UFO();
 
+	void Initialize(const std::string& name)override;
+
+	bool GetIsAlive()const;
+
+	// 更新
+	void Update()override;
+
+	// 衝突コールバック関数
+	void OnCollision([[maybe_unused]] Collider* other)override;
+
+	void RootInitialize();
+	void RootUpdate();
 private:
+	// 
+	// パラメータ
+	// 
+
+	// 速度
+	float speed_ = 0.001f;
+
+	bool isAlive_ = true;
+
+
+	// 現在のふるまい
+	Behavior behavior_ = Behavior::kRoot;
+	// 次のふるまいリクエスト
+	std::optional<Behavior> behaviorRequest_ = std::nullopt;
+
 
 };
