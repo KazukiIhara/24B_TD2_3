1cb5a116d28b64ec7da6efdbddaef56e77040932
KazukiIhara <iharakazuki0829@gmail.com>
2025-02-06 04:20:34 +0900

    一応チュートリアルシーンで自由に動かせるようにしました

--- project/application/scene/gameScenes/tutorialScene/TutorialScene.cpp
+++ project/application/scene/gameScenes/tutorialScene/TutorialScene.cpp
@@ -11,15 +11,164 @@ void TutorialScene::Initialize() {
 	//
 	moonMajarTurorial_ = std::make_unique<Object2DController>();
 	// オブジェクト作成
-	moonMajarTurorial_->Initialize(SUGER::Create2DObject("0_MM_Operation", "MoonMajorText/TutorialText_x1025y192.png"));
+	moonMajarTurorial_->Initialize(SUGER::Create2DObject("1_MM_Operation", "MoonMajorText/TutorialText_x1025y192.png"));
 	// 各種設定
 	moonMajarTurorial_->SetCutOutSize(moonMajarTextCutSize_);
 	moonMajarTurorial_->SetSize(moonMajarTextCutSize_);
 	moonMajarTurorial_->SetAnchorPoint(Vector2(0.5f, 0.5f));
 	moonMajarTurorial_->SetPosition(moonMajarTextPosition_);
-	moonMajarTurorial_->SetIsActive(false);
-
 	moonMajarTurorial_->SetIsActive(true);
+
+
+	//
+	// 天球の初期化処理
+	//
+
+	skydome_ = std::make_unique<Skydome>();
+	skydome_->Initialize(SUGER::CreateEntity("Skydome", "Skydome"));
+	skydome_->SetEnableLight(false);
+	skydome_->SetScale(100.0f);
+	skydome_->GetUVTransform().scale = { 10.0f,10.0f };
+
+	//
+	// 
+	//
+	damagePieceManager_ = std::make_unique<DamagePieceManager>();
+	damagePieceManager_->Initialize();
+
+
+	//
+	// Playerの初期化処理
+	//
+
+	player_ = std::make_unique<Player>();
+	player_->Initialize(SUGER::CreateEntity("Player", "Earth"));
+	// プレイヤーのコライダーを作成
+	player_->CreateCollider(ColliderCategory::Player, kSphere, 2.0f);
+	player_->GetCollider()->SetMass(20.0f);
+	player_->SetScale(2.0f);
+	player_->UpdateWorldTransform();
+
+
+	// 
+	// 月の初期化処理
+	// 
+
+	moon_ = std::make_unique<Moon>();
+	moon_->Initialize(SUGER::CreateEntity("Moon", "Moon"));
+	moon_->SetPlayer(player_.get());
+	moon_->CreateCollider(ColliderCategory::Moon, kSphere, 1.2f);
+	moon_->GetCollider()->SetMass(200.0f);
+	moon_->SetDamagePieceManager(damagePieceManager_.get());
+	moon_->UpdateWorldTransform();
+
+	moon_->SetParent(player_->GetLocalTransform());
+	player_->SetMoon(moon_.get());
+
+
+	// 
+	// ライトの初期化処理
+	// 
+
+	light_->GetPunctualLight().pointLight.position = ExtractionWorldPos(moon_->GetWorldTransformPtr()->worldMatrix_);
+	light_->GetPunctualLight().pointLight.intensity = 7.0f;
+	light_->GetPunctualLight().pointLight.color = { 1.0f,1.0f,0.0f,1.0f };
+	light_->GetPunctualLight().pointLight.radius = 10.0f;
+
+
+	light_->GetPunctualLight().spotLight.color = { 1.0f,1.0f,0.0f,1.0f };
+	light_->GetPunctualLight().spotLight.position = sceneCamera_->GetWorldPos();
+
+	light_->GetPunctualLight().spotLight.direction = Normalize(ExtractionWorldPos(moon_->GetWorldTransformPtr()->worldMatrix_) - sceneCamera_->GetWorldPos());
+	light_->GetPunctualLight().spotLight.intensity = 4.0f;
+	light_->GetPunctualLight().spotLight.decay = 3.0f;
+	light_->GetPunctualLight().spotLight.distance = 100.0f;
+	light_->GetPunctualLight().spotLight.cosAngle = 1.0f;
+	light_->GetPunctualLight().spotLight.cosFalloffStart = 0.99f;
+
+
+
+	// 
+	// かけらマネージャの初期化処理
+	// 
+
+	fragmentManager_ = std::make_unique<FragmentManager>();
+	fragmentManager_->Initialize();
+	fragmentManager_->SetEarth(moon_.get());
+	fragmentManager_->SetPlayer(player_.get());
+	// 
+	// 隕石マネージャの初期化処理
+	// 
+
+	meteoriteManager_ = std::make_unique<MeteoriteManager>();
+	meteoriteManager_->Initialize(moon_.get(), player_.get(), fragmentManager_.get(), damagePieceManager_.get());
+
+
+	//
+	// たんこぶマネージャーの初期化処理
+	//
+
+	bumpManager_ = std::make_unique<BumpManager>();
+	bumpManager_->Initialize();
+	bumpManager_->SetPlayer(player_.get());
+	bumpManager_->SetEarth(moon_.get());
+
+	player_->SetBumpManager(bumpManager_.get());
+
+	// 
+	// UFO弾マネージャの初期化
+	// 
+	ufoBulletManager_ = std::make_unique<UFOBulletManager>();
+	ufoBulletManager_->Initialize(player_.get());
+	ufoBulletManager_->SetDamagePieceManager(damagePieceManager_.get());
+
+	// 
+	// UFOマネージャの初期化処理
+	// 
+	ufoManager_ = std::make_unique<UFOManager>();
+	ufoManager_->Initialize(ufoBulletManager_.get());
+	ufoManager_->SetDamagePieceManager(damagePieceManager_.get());
+	ufoManager_->SetPlayer(player_.get());
+
+
+	// ボスの初期化処理
+	boss_ = std::make_unique<Boss>();
+	boss_->Initialize(SUGER::CreateEntity("Boss", "Boss"));
+	boss_->CreateCollider(ColliderCategory::Boss, kSphere, 6.0f);
+	boss_->GetCollider()->SetMass(20000.0f);
+	boss_->SetTranslate(bossPopPosition_);
+	boss_->UpdateWorldTransform();
+	boss_->SetUFOBulletManager(ufoBulletManager_.get());
+	boss_->SetPlayer(player_.get());
+
+	// 板ポリパーティクルの作成
+	SUGER::CreateParticle("dustParticle", ParticleType::kPlane, "circle.png");
+	//SUGER::CreateParticle("fragmentParticle", ParticleType::kModel,"Fragment");
+	SUGER::CreateParticle("fragmentParticle", ParticleType::kPlane, "dust.png");
+	SUGER::CreateParticle("earthDustParticle", ParticleType::kPlane, "dust.png");
+	SUGER::CreateParticle("explosionDustParticle", ParticleType::kPlane, "dust.png");
+	SUGER::CreateParticle("bumpParticle", ParticleType::kPlane, "circle.png");
+	SUGER::CreateParticle("ShotDustParticle", ParticleType::kPlane, "dust.png");
+	SUGER::CreateParticle("MoonParticle", ParticleType::kModel, "Moon");
+
+
+	moon_->SetPraticle();
+	moon_->UpdateWorldTransform();
+
+
+	//for (auto& tex : tutorialTexs_) {
+	//	tex = std::make_unique<Object2DController>();
+	//}
+	//tutorialTexs_[0]->Initialize(SUGER::Create2DObject("0_Tutorialtex", "TutorialImages/TutorialImage_00.png"));
+	//tutorialTexs_[1]->Initialize(SUGER::Create2DObject("0_Tutorialtex", "TutorialImages/TutorialImage_01.png"));
+	//tutorialTexs_[2]->Initialize(SUGER::Create2DObject("0_Tutorialtex", "TutorialImages/TutorialImage_02.png"));
+	//tutorialTexs_[3]->Initialize(SUGER::Create2DObject("0_Tutorialtex", "TutorialImages/TutorialImage_03.png"));
+	//tutorialTexs_[4]->Initialize(SUGER::Create2DObject("0_Tutorialtex", "TutorialImages/TutorialImage_04.png"));
+	//for (auto& tex : tutorialTexs_) {
+	//	tex->SetIsActive(false);
+	//}
+	//tutorialTexs_[0]->SetIsActive(true);
+
 
 }
 
@@ -33,9 +182,20 @@ void TutorialScene::SceneStatePlayInitialize() {
 
 void TutorialScene::SceneStatePlayUpdate() {
 
+	// ライトの座標
+	light_->GetPunctualLight().pointLight.position = ExtractionWorldPos(moon_->GetWorldTransformPtr()->worldMatrix_);
+	light_->GetPunctualLight().spotLight.direction = Normalize(ExtractionWorldPos(moon_->GetWorldTransformPtr()->worldMatrix_) - sceneCamera_->GetWorldPos());
+
+	player_->Update();
+	moon_->Update();
+	// 天球の更新
+	skydome_->Update();
+	// ダメージ破片の更新
+	damagePieceManager_->Update();
+
 	moonMajarTurorial_->SetLeftTop(Vector2(0.0f, static_cast<float>(currentPage_) * moonMajarTextCutSize_.y));
 	// ボタンでページ送り
-	if (SUGER::TriggerButton(0, ButtonA) || SUGER::TriggerKey(DIK_SPACE)) {
+	if (SUGER::TriggerButton(0, ButtonB) || SUGER::TriggerKey(DIK_SPACE)) {
 		currentPage_++;
 	}
 
--- project/application/scene/gameScenes/tutorialScene/TutorialScene.h
+++ project/application/scene/gameScenes/tutorialScene/TutorialScene.h
@@ -11,11 +11,14 @@
 #include "objects/player/Player.h"
 #include "objects/Earth/Moon.h"
 #include "objects/Skydome/Skydome.h"
+#include "objects/Boss/Boss.h"
 
 #include "system/MeteoriteManager/MeteoriteManager.h"
 #include "system/FragmentManager/FragmentManager.h"
 #include "system/BumpManager/BumpManager.h"
 #include "system/DamagePieceManager/DamagePieceManager.h"
+#include "system/UFOManager/UFOManager.h"
+#include "system/UFOBulletManager/UFOBulletManager.h"
 
 enum class MMTutorial {
 	Operation,
@@ -43,6 +46,36 @@ public:
 	void SceneStateFadeOutUpdate()override;
 
 private:
+
+	// プレイヤー
+	std::unique_ptr<Player> player_ = nullptr;
+	// 月
+	std::unique_ptr<Moon> moon_ = nullptr;
+
+	float inclination_ = 23.4f;
+
+	// 天球
+	std::unique_ptr<Skydome> skydome_ = nullptr;
+	// ダメージ破片
+	std::unique_ptr<DamagePieceManager> damagePieceManager_ = nullptr;
+
+	// ボス
+	std::unique_ptr<Boss> boss_ = nullptr;
+	Vector3 bossPopPosition_ = { 45.0f,0.0f,0.0f };
+	Vector3 bossBattleBeginPosition_ = { 25.0f,0.0f,0.0f };
+
+	// 隕石マネージャ
+	std::unique_ptr<MeteoriteManager> meteoriteManager_ = nullptr;
+	// かけらマネージャ
+	std::unique_ptr<FragmentManager> fragmentManager_ = nullptr;
+	// たんこぶマネージャー
+	std::unique_ptr<BumpManager> bumpManager_ = nullptr;
+	// UFOマネージャ
+	std::unique_ptr<UFOManager> ufoManager_ = nullptr;
+	// UFO弾マネージャ
+	std::unique_ptr<UFOBulletManager> ufoBulletManager_ = nullptr;
+
+
 	//
 	// ムーン少佐スプライト
 	//
@@ -57,4 +90,7 @@ private:
 	uint32_t tutorialPages_ = 15;
 	// 現在のページ
 	uint32_t currentPage_ = 0;
+
+	std::array<std::unique_ptr<Object2DController>, 5> tutorialTexs_;
+
 };
